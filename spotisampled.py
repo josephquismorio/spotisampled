0922579fbf2f66c6c7930f86418a003d78871207
joey quismorio <70463608+josephquismorio@users.noreply.github.com>
2021-11-18 01:53:55 -0600

    general code cleanup business
    yeah

--- spotisampled.py
+++ spotisampled.py
@@ -8,72 +8,31 @@ from secrets import clientID
 from secrets import clientSecret
 from secrets import username
 
-# endpoint #
 authURL = "https://accounts.spotify.com/api/token" 
 
-# authorization header for credential specification #
 authHeader = {}
 
-# hold grant type for access token #
 authData = {}
 
-# base64 encoder for app client ID and secret #
 def accessToken(clientID, clientSecret):
     message = f"{clientID}:{clientSecret}" 
     messageBytes = message.encode('ascii')
     base64bytes = base64.b64encode(messageBytes)
     base64message = base64bytes.decode('ascii')
-
-    # create authorization tag for header with encoded message #
     authHeader['Authorization'] = "Basic " + base64message
-
-    # create grant type tag #
     authData['grant_type'] = "client_credentials"
-
-    # !!! be sure to have requests library installed correctly !!! #
-    # !!! have cacert.pem copied to your certifications folder !!! #
     res = requests.post(authURL, headers=authHeader, data=authData)
-
-    # print(base64message) 
-
-    # print(res)
-
-    # create access token #
     resObj = res.json()
-
     token = resObj['access_token']
-    
     return token
 
-
-
 def playlistInfo(token, playlistID):
-    # referenced from API endpoint reference #
     playlist = f"https://api.spotify.com/v1/playlists/{playlistID}"
-    
-    # get authorization header #
     getHeader = {"Authorization": "Bearer " + token}
-    
     res = requests.get(playlist, headers=getHeader)
-    
     playlistObj = res.json()
-    
     return playlistObj
 
-# API request #
-token = accessToken(clientID, clientSecret)
-
-playlistURL = input("Please enter playlist URL: ")
-
-playlistID = playlistURL.replace("https://open.spotify.com/playlist/", "")
-
-tracklist = playlistInfo(token, playlistID)
-
-# write data into json file for extraction later #
-with open('tracklist.json', 'w') as f:
-    json.dump(tracklist, f)
-
-# extract data from json file #
 def exportFormattedPlaylist(tracklist):
     titleList = []
     artistList = []
@@ -84,8 +43,6 @@ def exportFormattedPlaylist(tracklist):
         titleList.append(title)
     return titleList, artistList
 
-titles, artists = exportFormattedPlaylist(tracklist)
-
 def checkSampled(a, t):
     query = f"{a.replace(' ', '%20')}%20-%20{t.replace(' ', '%20')}"
     url = 'https://www.whosampled.com/search/tracks/?q={}'.format(query)
@@ -95,8 +52,6 @@ def checkSampled(a, t):
     if result:
         link = [i.a for i in result][0].get('href')
         return link
-#    else:
-#       print(f'{a} - {t} not found on WhoSampled') 
 
 def getSamples(t, link):
     samples = []
@@ -137,7 +92,6 @@ def samplesPlaylist(titleList, artistList):
     newList = [i for j in samplesPlaylist for i in j]
     return newList
 
-
 def findSamplesSpotify(newList):
     ccm = util.prompt_for_user_token(username, scope='playlist-modify-public', client_id=clientID, client_secret=clientSecret, redirect_uri='http://127.0.0.1.8080/')
     sp = spotipy.Spotify(auth=ccm)
@@ -152,34 +106,42 @@ def findSamplesSpotify(newList):
                 subList.append(i['id'])
                 break
         if subList:
-#           print(f"Found {track['title']} by {track['artist']} on Spotify")
             found.append(subList[0])
         else:
             notFound.append((track['title'] + ' by ' + track['artist']))
     location_rate = 1 - len(notFound)/len(newList)
     return {'ids': found, 'unfound': notFound, 'rate': location_rate}
 
-
-def setupNewPlaylist(newList, playlistName):
+def setupNewPlaylist(newList):
     ccm = util.prompt_for_user_token(username, scope='playlist-modify-public', client_id=clientID, client_secret=clientSecret, redirect_uri='http://127.0.0.1.8080/')
     sp = spotipy.Spotify(auth=ccm)
     allSamples = findSamplesSpotify(newList) 
-    desc = f"Enjoy! I was unable to find: {len(allSamples['unfound'])} samples."
-    playlist = sp.user_playlist_create(username, playlistName, public=True, description=desc)
+    name = "Spotisample Playlist"
+    desc = f"Enjoy! Unable to find {len(allSamples['unfound'])} samples on Spotify."
+    playlist = sp.user_playlist_create(username, name, public=True, description=desc)
     playlistID = sp.user_playlists(user=username)['items'][0]['id']
     sp.user_playlist_add_tracks(username, playlistID, allSamples['ids'], None)
     pass
 
-def createNewPlaylist(newList, playlistName):
+def createNewPlaylist(newList):
     ccm = util.prompt_for_user_token(username, scope='playlist-modify-public', client_id=clientID, client_secret=clientSecret, redirect_uri='http://127.0.0.1.8080/')
     sp = spotipy.Spotify(auth=ccm)
     samples = findSamplesSpotify(newList)
-    new_playlist = setupNewPlaylist(newList, playlistName)
-    print(f'New playlist "{playlistName}" created!')
+    new_playlist = setupNewPlaylist(newList)
+    print("-----------------------------------------")
+    print(f'New playlist created! Was unable to find:')
+    for i in samples['unfound']:
+        print(i)
     pass
 
 def run():
-    name = input('Please enter a name for the sample playlist: ')
-    createNewPlaylist(samplesPlaylist(titles, artists), name)
+    token = accessToken(clientID, clientSecret)
+    playlistURL = input("Please enter playlist URL: ")
+    playlistID = playlistURL.replace("https://open.spotify.com/playlist/", "")
+    tracklist = playlistInfo(token, playlistID)
+    with open('tracklist.json', 'w') as f:
+        json.dump(tracklist, f)
+    titles, artists = exportFormattedPlaylist(tracklist)
+    createNewPlaylist(samplesPlaylist(titles, artists))
     
 run()
